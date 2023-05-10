3a812a48f927f942451dad34134fa43fc8b3d8d7
abner <fantasrulai@gmail.com>
2023-05-10 22:25:29 +0800

    add gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -54,28 +54,37 @@ class CustomDocument extends Document {
             property="og:image"
             content="https://niconite-frontend.vercel.app/profile.jpg"
           />
-          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TMJQMLH');`}</script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TMJQMLH');`,
+            }}
+          ></script>
           {/* <!-- Google tag (gtag.js) --> */}
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-6HD3F19P07"
           ></script>
-          <script>
-            {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-6HD3F19P07');`}
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6HD3F19P07');`,
+            }}
+          ></script>
         </Head>
         <body>
           {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>{`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMJQMLH"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`}</noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMJQMLH"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
